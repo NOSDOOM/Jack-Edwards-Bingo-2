e7d01f8a58973ce0e8d98d6e7c85ad72979e1bd5
Bane89 <edgepunk06@gmail.com>
2015-11-17 10:08:26 -0600

    Added Konami code

---
+++ README.txt
@@ -0,0 +1,4 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/country_runner/pen/WQYOxB.
+Baised off JeffeHobbs Bingo https://github.com/jeffehobbs/HTML5-bingo
+
+ 
---
+++ js/data.js
@@ -0,0 +1,43 @@
+var headerText = "Jack Edwards Bingo";
+
+var footerText = "<a Jack Edwards Bingo</a>";
+
+var winText = "Winner";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "OFF GLASS"},
+        {"square": "TAKES THE HIT TO MOVE THE PUCK"},
+        {"square": "INTO THE EDDY OF THE FLOW"},
+        {"square": "EXECUTIVE DESK TOY HIT"},
+        {"square": "SAVE BY RASK!"},
+        {"square": "FLIPS THE CAN OF CORN"},
+        {"square": "[PLAYER NAME] SAYS 'HOW DO YOU DO?'"},
+        {"square": "I THOUGHT I HEARD POST!"},
+        {"square": "STICKS A FORK IN IT!"},
+        {"square": "TUMBLING MUFFIN"},
+        {"square": "HE SPOONS IT"},
+        {"square": "OTHER TEAM SCOORES"},
+        {"square": "BLOWS A TIRE"},
+        {"square": "D TO D"},
+        {"square": "SAUCER PASS"},
+        {"square": "IT'S WINNING TIME"},
+        {"square": "MID-AIR FUNGO JOB"},
+        {"square": "DOWN THE RIVER"},
+        {"square": "COCKS AND FIRES"},
+        {"square": "I LEARNED THIS FROM YOU, BRICK"},
+        {"square": "JUICY REBOUND"},
+        {"square": "CHINESE MUSTARD"},
+        {"square": "CARRIES INTO THE ZONE PUTS BRAKES ON"},
+        {"square": "TRIANGLES"},    	
+        {"square": "SETTING UP IN GRETZKY'S OFFICE"},    	
+        {"square": "2 U'S 2K'S 2 POINTS!"},    	
+        {"square": "HE'S GOT A BEE IN HIS BONNET"}, 
+        {"square": "HE GOT UNDRESSED"},    	
+        {"square": "BANGS"},    	
+        {"square": "MARTHA, THE BARN IS ON FIRE!"}    	
+    ]
+};
